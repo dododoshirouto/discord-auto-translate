cd1a390f6985c164b17db142840512ae35ab2289
どどど素人 <87510462+dododoshirouto@users.noreply.github.com>
2022-09-14 21:34:28 +0900

    Create tampermonkey-script.js

---
+++ tampermonkey-script.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Discord Auto Translate
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  auto translate.
+// @author       @super_amateur_c
+// @match        https://discord.com/channels/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=discord.com
+// @grant        none
+// ==/UserScript==
+
+function tm-init() {
+    
+}
